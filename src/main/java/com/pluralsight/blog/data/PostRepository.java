e7d6bab74f56c0116cce5c99943d7d05692aa1b8
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-06-19 14:03:47 -0400

    clarification: data will load from data.sql file in src/main/resources into the database

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -4,6 +4,7 @@ import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // expose basic CRUD operations for Post Model
+// Spring will load Post data from data.sql file into a database on app startup
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 }
